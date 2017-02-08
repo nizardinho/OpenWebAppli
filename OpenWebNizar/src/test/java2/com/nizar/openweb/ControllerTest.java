e11ab636be39f531f9b5c6ece4fb063d607916d4
Programme <nizarmabrouki@outlook.com>
2017-02-08 17:02:04 +0100

    Ajout dépendance de test + modification test unitaire

--- OpenWebNizar/src/test/java2/com/nizar/openweb/ControllerTest.java
+++ OpenWebNizar/src/test/java2/com/nizar/openweb/ControllerTest.java
@@ -1,6 +1,6 @@
 package com.nizar.openweb;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -8,91 +8,6 @@ public class ControllerTest {
 
     @Test
     public final void testRegistrationModel() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testRegistrationUserBindingResultModel() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testLogin() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testWelcome() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testAddBoatUserBoatString() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testAddBoatModel() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testObject() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testGetClass() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testHashCode() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testEquals() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testClone() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testToString() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testNotify() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testNotifyAll() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testWaitLong() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testWaitLongInt() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testWait() {
-        fail( "Pas encore implémenté" ); // TODO
-    }
-
-    @Test
-    public final void testFinalize() {
         fail( "Pas encore implémenté" ); // TODO
     }
 
